a984d5435823a8ce90442858a6baafa87476b32a
Joel Pålsson <jpa@hms.se>
2016-09-16 10:49:24 +0200

    Added firmware upgrade in progress attribute.
    Added attribute that indicates to host applicaiton if firmware upgrade
    of safety module is currently in progress.
    
    Change-Id: I36fe4a789651417a21551be97ae411ede3c981d2

--- inc/abp_safe.h
+++ inc/abp_safe.h
@@ -50,12 +50,13 @@
 **------------------------------------------------------------------------------
 */
 
-#define ABP_SAFE_IA_SAFETY_ENABLED        1
-#define ABP_SAFE_IA_BAUD_RATE             2
-#define ABP_SAFE_IA_IO_CONFIG             3 /* Obsolete - replaced by command
-                                             * in functional safety module
-                                             * object. */
-#define ABP_SAFE_IA_CYCLE_TIME            4 /* ABCC40 */
+#define ABP_SAFE_IA_SAFETY_ENABLED              1
+#define ABP_SAFE_IA_BAUD_RATE                   2
+#define ABP_SAFE_IA_IO_CONFIG                   3 /* Obsolete - replaced by command
+                                                   * in functional safety module
+                                                   * object. */
+#define ABP_SAFE_IA_CYCLE_TIME                  4 /* ABCC40 */
+#define ABP_SAFE_IA_FW_UPGRADE_IN_PROGRESS      5 /* ABCC40 */
 
 
 /*------------------------------------------------------------------------------
@@ -65,9 +66,10 @@
 **------------------------------------------------------------------------------
 */
 
-#define ABP_SAFE_IA_SAFETY_ENABLED_DS     ABP_BOOL_SIZEOF
-#define ABP_SAFE_IA_BAUD_RATE_DS          ABP_UINT32_SIZEOF
-#define ABP_SAFE_IA_CYCLE_TIME_DS         ABP_UINT8_SIZEOF
+#define ABP_SAFE_IA_SAFETY_ENABLED_DS           ABP_BOOL_SIZEOF
+#define ABP_SAFE_IA_BAUD_RATE_DS                ABP_UINT32_SIZEOF
+#define ABP_SAFE_IA_CYCLE_TIME_DS               ABP_UINT8_SIZEOF
+#define ABP_SAFE_IA_FW_UPGRADE_IN_PROGRESS_DS   ABP_BOOL_SIZEOF
 
 
 #endif  /* inclusion lock */
