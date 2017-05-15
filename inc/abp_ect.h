c93331354582c822fb22f518f2f5d1010d92a0e4
Håkan Högfeldt <haho@hms.se>
2017-05-15 15:17:48 +0200

    Added attribute "Compare identity lists" to EtherCAT object.
    Resolves issue m#18034.
    
    Change-Id: I8c19411ad5f39ecaf8a7f4258dac5af5dd2357bf

--- inc/abp_ect.h
+++ inc/abp_ect.h
@@ -100,6 +100,7 @@
 #define ABP_ECT_IA_SET_DEV_ID_AS_CSA      19
 #define ABP_ECT_IA_ETHERCAT_STATE         20
 #define ABP_ECT_IA_STATE_TIMEOUTS         21
+#define ABP_ECT_IA_COMP_IDENT_LISTS       22
 
 
 /*------------------------------------------------------------------------------
@@ -131,6 +132,7 @@
 #define ABP_ECT_IA_SET_DEV_ID_AS_CSA_DS         ABP_BOOL_SIZEOF
 #define ABP_ECT_IA_ETHERCAT_STATE_DS            ABP_UINT8_SIZEOF
 #define ABP_ECT_IA_STATE_TIMEOUTS_DS            ( ABP_UINT32_SIZEOF * 4 )
+#define ABP_ECT_IA_COMP_IDENT_LISTS_DS          ABP_BOOL_SIZEOF
 
 
 /*------------------------------------------------------------------------------
