f57ff965e0659abe816bd738570e02759fcbfb21
Rikard Dyrsch <rdy@hms.se>
2018-04-27 10:51:37 +0200

    Added MQTT last will configuration instance attribute
    A8070-134
    
    Change-Id: Ie70f2630222543c0da80bc99bb833f2814ecc832

--- inc/abp_mqtt.h
+++ inc/abp_mqtt.h
@@ -28,12 +28,14 @@
 **------------------------------------------------------------------------------
 */
 #define ABP_MQTT_IA_MODE        ( 1 )
+#define APB_MQTT_IA_LAST_WILL   ( 2 )
 
 /*------------------------------------------------------------------------------
 ** The data size of the MQTT instance attributes
 **------------------------------------------------------------------------------
 */
-#define ABP_MQTT_IA_MODE_DS     ( ABP_UINT8_SIZEOF )
+#define ABP_MQTT_IA_MODE_DS            ( ABP_UINT8_SIZEOF )
+#define ABP_MQTT_IA_LASTWILL_MAX_DS    ( 390 * ABP_OCTET_SIZEOF )
 
 /*------------------------------------------------------------------------------
 ** Values of the Mode attribute
@@ -45,6 +47,17 @@ typedef enum ABP_MqttMode
    ABP_MQTT_MODE_JSON     = 1
 }
 ABP_MqttModeType;
+
+/*------------------------------------------------------------------------------
+** Last will instance attribute specific defines
+**------------------------------------------------------------------------------
+*/
+#define ABP_MQTT_LASTWILL_QOS_OFFSET                    ( 0 )
+#define ABP_MQTT_LASTWILL_RETAIN_OFFSET                 ( 1 )
+#define ABP_MQTT_LASTWILL_TOPIC_LENGTH_OFFSET           ( 2 )
+#define ABP_MQTT_LASTWILL_TOPIC_STR_OFFSET              ( 4 )
+#define ABP_MQTT_LASTWILL_MESSAGE_LENGTH_BASE_OFFSET    ( 4 )
+#define ABP_MQTT_LASTWILL_MESSSAGE_STR_BASE_OFFSET      ( 6 )
 
 /*------------------------------------------------------------------------------
 ** The MQTT Object specific message commands
