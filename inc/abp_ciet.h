49dbdc813b2776ed2d0604c55a8849a7650cee6a
Martin Åkesson <bemartin@hms.se>
2024-10-24 09:14:01 +0200

    Add CIET Clock Offset attribute defines
    Change-Id: If10b407f2fd892896c0bafc48ff50294f8fa17ff

--- inc/abp_ciet.h
+++ inc/abp_ciet.h
@@ -40,6 +40,7 @@
 #define ABP_CIET_IA_FW_VERSION           8
 #define ABP_CIET_IA_HW_VERSION           9
 #define ABP_CIET_IA_SERIAL_NUMBER        10
+#define ABP_CIET_IA_CLOCK_OFFSET         11
 
 /*------------------------------------------------------------------------------
 ** The data size of the CC-Link IE TSN object instance attributes
@@ -60,5 +61,25 @@
 #define ABP_CIET_IA_FW_VERSION_DS           ABP_UINT16_SIZEOF
 #define ABP_CIET_IA_HW_VERSION_DS           ABP_UINT8_SIZEOF
 #define ABP_CIET_IA_SERIAL_NUMBER_DS        ( ABP_CHAR_SIZEOF * ABP_CIET_SERIAL_NUMBER_STR_LEN )
+#define ABP_CIET_IA_CLOCK_OFFSET_DS         ( ABP_UINT64_SIZEOF + ABP_UINT32_SIZEOF + 2 * ABP_UINT16_SIZEOF )
+
+/*******************************************************************************
+** Typedefs
+********************************************************************************
+*/
+/*------------------------------------------------------------------------------
+** Structure describing clock offset
+**------------------------------------------------------------------------------
+*/
+ABCC_SYS_PACK_ON
+typedef struct ABP_CIET_ClockOffset
+{
+   INT64 llOffsetSec;
+   INT32 lOffsetNanoSec;
+   INT16 iUtcOffsetMin;
+   INT16 iSummerTimeOffsetMin;
+}
+PACKED_STRUCT ABP_CIET_ClockOffsetType;
+ABCC_SYS_PACK_OFF
 
 #endif  /* inclusion lock */
